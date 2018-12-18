e6d88fe4eec01240c80ead558efaf9f9cf08a4e2
Yasuaki Uechi <y@uechi.io>
2018-12-18 10:49:24 +0900

    add duplexImage

--- base256.py
+++ base256.py
@@ -2,11 +2,12 @@
 # usage: python3 base256.py text
 
 import sys
+import math
 from PIL import Image, ImageDraw
 
 encodingBit = 8  # 2^8 = 256
 pixelWidth = 10
-pixelHeight = 100
+pixelHeight = 50
 
 
 def encode(text):
@@ -24,6 +25,23 @@ def createImage(data, outfile='output.png'):
         draw.rectangle(
             (i * pixelWidth, 0, i * pixelWidth + pixelWidth, pixelHeight),
             fill=(n, 0, 0))
+    img.save(outfile)
+
+
+def createDuplexImage(data, outfile='output.png'):
+    rowLength = math.ceil(len(data) / 3)
+    img = Image.new('RGB', (rowLength * pixelWidth, pixelHeight), color='black')
+    draw = ImageDraw.Draw(img)
+    for i, n in enumerate(data):
+        rgbIndex = math.floor(i / rowLength)
+        rowIndex = i % rowLength
+        print(rgbIndex, rowIndex)
+        pixel = list(img.getpixel((rowIndex, 0)))
+        pixel[rgbIndex] = n
+        print(pixel)
+        draw.rectangle((rowIndex * pixelWidth, 0,
+                        rowIndex * pixelWidth + pixelWidth, pixelHeight),
+                       fill=tuple(pixel))
     img.save(outfile)
 
 
