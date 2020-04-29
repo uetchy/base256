a7f796ba07ac70de4a073cb0ac396c2dbe087548
Yasuaki Uechi <y@uechi.io>
2020-04-29 13:40:52 +0900

    cosmetic changes

--- base256.py
+++ base256.py
@@ -9,101 +9,101 @@ import random
 from PIL import Image, ImageDraw
 
 ENCODING_BIT = 8  # 2^8 = 256
-PIXEL_WIDTH = 10
-PIXEL_HEIGHT = 50
+PIXEL_WIDTH = 64
+PIXEL_HEIGHT = 512
 
 
 def encode(text):
-  """
+    """
   Encode to base64
   """
 
-  return [int(x) for x in text.encode('utf8')]
+    return [int(x) for x in text.encode('utf8')]
 
 
 def decode(index_map):
-  """
+    """
   Decode from base64
   """
-  return bytes(index_map).decode('utf8')
+    return bytes(index_map).decode('utf8')
 
 
 def create_image(data, outfile='output.png'):
-  """
+    """
   Generate and save image
   """
-  img = Image.new('RGB', (len(data) * PIXEL_WIDTH, PIXEL_HEIGHT), color='black')
-  draw = ImageDraw.Draw(img)
-  for i, grad in enumerate(data):
-    draw.rectangle((i * PIXEL_WIDTH, 0, i * PIXEL_WIDTH + PIXEL_WIDTH, PIXEL_HEIGHT),
-                   fill=(grad, 0, 0))
-  img.save(outfile)
+    img = Image.new('RGB', (len(data) * PIXEL_WIDTH, PIXEL_HEIGHT), color='black')
+    draw = ImageDraw.Draw(img)
+    for i, grad in enumerate(data):
+        draw.rectangle((i * PIXEL_WIDTH, 0, i * PIXEL_WIDTH + PIXEL_WIDTH, PIXEL_HEIGHT),
+                       fill=(grad, 0, 0))
+    img.save(outfile)
 
 
-def random_filler(_):
-  """
+def random_filler(grad):
+    """
   Random Filter
   """
-  filler = lambda: math.floor(random.random() * 2) * 125
-  return (filler(), filler(), filler())
+    filler = lambda: math.floor(random.random() * 2) * 125
+    return (grad, filler(), filler())
 
 
 def yellow_filler(grad):
-  """
+    """
   Yellow Filter
   """
-  return (grad, grad, 0)
+    return (grad, grad, 0)
 
 
 def create_square_image(data, outfile='output.png'):
-  """
+    """
   Generate square image
   """
 
-  image_length = math.ceil(math.sqrt(len(data)))
-  pixel_length = 128
-  length = image_length * pixel_length
+    image_length = math.ceil(math.sqrt(len(data)))
+    pixel_length = 128
+    length = image_length * pixel_length
 
-  img = Image.new('RGB', (length, length), color='black')
-  draw = ImageDraw.Draw(img)
-  for i, grad in enumerate(data):
-    row_index = math.floor(i / image_length)
-    column_index = i % image_length
-    draw.rectangle(
-        ((column_index * pixel_length, row_index * pixel_length),
-         (column_index * pixel_length + pixel_length, row_index * pixel_length + pixel_length)),
-        fill=yellow_filler(grad))
-  img.save(outfile)
+    img = Image.new('RGB', (length, length), color='black')
+    draw = ImageDraw.Draw(img)
+    for i, grad in enumerate(data):
+        row_index = math.floor(i / image_length)
+        column_index = i % image_length
+        draw.rectangle(
+            ((column_index * pixel_length, row_index * pixel_length),
+             (column_index * pixel_length + pixel_length, row_index * pixel_length + pixel_length)),
+            fill=random_filler(grad))
+    img.save(outfile)
 
 
 def create_duplex_image(data, outfile='output.png'):
-  """
+    """
   Create duplex image
   """
 
-  row_length = math.ceil(len(data) / 3)
-  img = Image.new('RGB', (row_length * PIXEL_WIDTH, PIXEL_HEIGHT), color='black')
-  draw = ImageDraw.Draw(img)
-  for i, grad in enumerate(data):
-    rgb_index = math.floor(i / row_length)
-    row_index = i % row_length
-    print(rgb_index, row_index)
-    pixel = list(img.getpixel((row_index, 0)))
-    pixel[rgb_index] = grad
-    print(pixel)
-    draw.rectangle(
-        (row_index * PIXEL_WIDTH, 0, row_index * PIXEL_WIDTH + PIXEL_WIDTH, PIXEL_HEIGHT),
-        fill=tuple(pixel))
-  img.save(outfile)
+    row_length = math.ceil(len(data) / 3)
+    img = Image.new('RGB', (row_length * PIXEL_WIDTH, PIXEL_HEIGHT), color='black')
+    draw = ImageDraw.Draw(img)
+    for i, grad in enumerate(data):
+        rgb_index = math.floor(i / row_length)
+        row_index = i % row_length
+        print(rgb_index, row_index)
+        pixel = list(img.getpixel((row_index, 0)))
+        pixel[rgb_index] = grad
+        print(pixel)
+        draw.rectangle(
+            (row_index * PIXEL_WIDTH, 0, row_index * PIXEL_WIDTH + PIXEL_WIDTH, PIXEL_HEIGHT),
+            fill=tuple(pixel))
+    img.save(outfile)
 
 
 if __name__ == '__main__':
-  print('Base' + str(2**ENCODING_BIT))
-  SOURCE = sys.argv[1]
-  print(SOURCE)
-  with open(SOURCE) as f:
-    SOURCE = ''.join(f.readlines())
-  ENCODED = encode(SOURCE)
-  create_square_image(ENCODED)
-  DECODED = decode(ENCODED)
-  print('復元テスト: ' + DECODED)
+    print('Base' + str(2**ENCODING_BIT))
+    SOURCE = sys.argv[1]
+    print(SOURCE)
+    with open(SOURCE) as f:
+        SOURCE = ''.join(f.readlines())
+    ENCODED = encode(SOURCE)
+    create_image(ENCODED)
+    DECODED = decode(ENCODED)
+    print('復元テスト: ' + DECODED)
--- decode.py
+++ decode.py
@@ -7,25 +7,29 @@ import numpy as np
 
 
 def decode(index_map):
-  """
-  Decode string
-  """
-  return bytes(index_map).decode('utf8')
+    """
+    Decode string
+    """
+    return bytes(index_map).decode('utf8')
 
 
-def decode_image(image, stride=30):
-  """
-  Decode image
-  """
-  data = np.array(image)
-  row = list(data[0, :, 0])  # 0, :, 0 for red pixels
-  index_map = [row[x] for x in range(0, len(row), stride)]
-  print(index_map)
-  return decode(index_map)
+def detect_stride(vec):
+    return len([x for x in vec if x == vec[0]])
+
+
+def decode_image(image):
+    """
+    Decode image
+    """
+    image = image.convert('RGB')
+    data = np.array(image)
+    row = list(data[0, :, 0])  # 0, :, 0 for red pixels
+    stride = detect_stride(row)
+    index_map = [row[x] for x in range(0, len(row), stride)]
+    return decode(index_map)
 
 
 if __name__ == '__main__':
-  SOURCE = sys.argv[1]
-  STRIDE = int(sys.argv[2])
-  DATA = decode_image(Image.open(SOURCE), STRIDE)
-  print(DATA)
+    SOURCE = sys.argv[1]
+    DATA = decode_image(Image.open(SOURCE))
+    print(DATA)
